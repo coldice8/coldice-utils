0076761cf31f76342731964af9e95f2d30949a32
YX-CUIHB <coldice945@hotmail.com>
2019-04-02 15:01:33 +0800

    增加格式化时分秒的工具函数；

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import deviceInfo from './device';
+import {
+  getUrlParams,
+  isExternal,
+  formatSecond,
+} from './utils';
 
 export {
   deviceInfo, // 获取设备信息
   getUrlParams, // 获取 url 中的 query 参数
-  isExternal, //判断是否是外部链接
+  isExternal, // 判断是否是外部链接
+  formatSecond, // 格式化时分秒
 };
--- utils.js
+++ utils.js
@@ -50,3 +50,23 @@ export function isExternal(url) {
 
   return host !== linkHost;
 }
+
+/**
+ * 格式化时分秒
+ * @param result
+ * @returns {string}
+ */
+export function formatSecond(result) {
+  const h = Math.floor((result / 3600) % 24);
+  const m = Math.floor((result / 60) % 60);
+  const s = Math.floor(result % 60);
+  result = s + "秒";
+  if (m > 0) {
+    result = m + "分" + result;
+  }
+  if (h > 0) {
+    result = h + "小时" + result;
+  }
+
+  return result;
+}
